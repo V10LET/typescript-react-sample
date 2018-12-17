94d099f7fef642e9349ee03f8c576c355b6ff652
violet moon <violet.wmoon@gmail.com>
2018-12-16 18:40:53 -0600

    added button for more practice

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,5 +1,28 @@
 import * as React from 'react'
 
-export interface HelloProps { compiler: string, framework: string}
+interface Props {compiler: string, framework: string}
+interface State {color: string}
 
-export const Hello = (props: HelloProps) => <h1>Hello from {props.compiler} and {props.framework}!</h1>
+const colors:string[] = ['red', 'blue', 'green', 'purple', 'pink', 'orange']
+
+export class Hello extends React.Component<Props, State> {
+
+  state = {
+    color: 'black'
+  }
+
+  handleClick = () => {
+    const color = colors[Math.floor(Math.random() * colors.length)]
+    this.setState({color})
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 style={{color: this.state.color}}>Hello from {this.props.compiler} and {this.props.framework}!</h1>
+        <button onClick={this.handleClick}>change color</button>
+      </div>
+    )
+  }
+
+}
